0c64119f4f97bd85e55e6cf13f39918605fb32f3
Daniel Sánchez <danisanchezmunoz3@gmail.com>
2025-04-14 12:50:32 +0200

    Cathup a true y coreccion de zona horaria de actuañizacion

--- dags/news_dag.py
+++ dags/news_dag.py
@@ -1,15 +1,15 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from datetime import datetime
+import pendulum
 
 from ConvotisPractica.newsapi_extract import extract_news
 from ConvotisPractica.load_to_snowflake import load_to_snowflake
 
 with DAG(
     dag_id="news_dag",
-    start_date=datetime(2025, 4, 1),
+    start_date=pendulum.datetime(2025, 4, 1, tz="Europe/Madrid"),
     schedule_interval="@daily",
-    catchup=False
+    catchup=True
 ) as dag:
 
     task_extract = PythonOperator(
